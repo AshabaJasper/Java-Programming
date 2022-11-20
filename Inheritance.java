93cea4db6c43b8a40a5e53813c2f9dc6380bf440
AshabaJasper <ashabajasper@gmail.com>
2022-11-20 16:19:14 +0300

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -10,8 +10,20 @@ class Product{
     }
 
     //methods
-    void setProductDetails
+    void setProductDetails(int p_id, String name, int price){
+        //write data in product object
+        this.p_id=p_id;
+        this.name=name;
+        this.price=price;
+    }
+    void showProductDetails(){
+        //read data from product object
+        System.out.println("-----Product ID: "+p_id+"-----");
+        System.out.println("Name:\t"+name);
+        System.out.println("Price:\t"+price);
+        System.out.println("---------------------------------")
 
+    }
 }
 
 
@@ -21,7 +33,8 @@ public class Inheritance {
     //main is executed by JVM when program is running
 
     public static void main(String[] args){
-
+    Product product= new Product();
+    System.out.println("Product is: "+product);
 
 
     }
