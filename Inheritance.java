b45b82070bda54bcdbc5a103062195538d3470b5
AshabaJasper <ashabajasper@gmail.com>
2022-11-20 16:03:50 +0300

    Update

--- Inheritance.java
+++ Inheritance.java
@@ -1,2 +1,19 @@
+class Product{
+    //attributes
+    int p_id;
+
+
+}
+
+
+
 public class Inheritance {
+
+    //main is executed by JVM when program is running
+
+    public static void main(String[] args){
+
+
+
+    }
 }
