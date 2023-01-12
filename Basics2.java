78e449efcea24f2fa874d1a81c609945db2d0531
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2023-01-12 17:28:35 +0300

    Update Basics2.java

--- Basics2.java
+++ Basics2.java
@@ -6,7 +6,7 @@ public class Basics {
     // Declare and initialize a two-dimensional array of type int
     int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
-    // Print the elements of the array
+    // Print the elements of the arrays
     for (int i = 0; i < matrix.length; i++) {
       for (int j = 0; j < matrix[i].length; j++) {
         System.out.print(matrix[i][j] + " ");
